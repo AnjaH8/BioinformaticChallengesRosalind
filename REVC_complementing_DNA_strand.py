e3d2b837bf98f85a30dbfbf9106d6fb705ee7ab0
Anja Herdtfelder <Anja@Herdtfelder.de>
2022-10-27 20:58:56 +0200

    Initial commit, everything works

---
+++ REVC_complementing_DNA_strand.py
@@ -0,0 +1,27 @@
+# exercise from https://rosalind.info/problems/revc/
+import sys
+def main(argv):
+    # Check if there is an argument
+    if argv == None:
+        print("Usage: REVC_complementing_DNA_strand.py <DNA-sequence>")
+
+    argv = argv.lower()
+    # Complement the DNA strand 3' to 5'
+    complement = ""
+    for nt in range(len(argv)-1, -1, -1):
+        if argv[nt] == "a":
+            complement += "T"
+        elif argv[nt] == "t":
+            complement += "A"
+        elif argv[nt] == "g":
+            complement += "C"
+        elif argv[nt] == "c":
+            complement += "G"
+        else:
+            sys.exit("Error: Only ACGT allowed.")
+    print(complement)
+    return 0
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
